cec3cb30fddc6de23c25f0163d0ec529b6ce1cb8
nshderrick056 <d.gatete@alustudent.com>
2026-02-22 18:15:15 +0200

    creating the users table

--- database/schema.sql
+++ database/schema.sql
@@ -68,3 +68,11 @@ CREATE TABLE IF NOT EXISTS trips (
     FOREIGN KEY (dropoff_time_id) REFERENCES time_dim(time_id)
 
 );
+
+-- 6. Authentication: USERS
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
